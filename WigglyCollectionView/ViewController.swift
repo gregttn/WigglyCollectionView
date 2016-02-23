b23857567199aaf66fa7b48f5b12d3cd891d661e
gregttn <gregttn@gmail.com>
2016-02-23 19:19:37 +0000

    Initial Commit

---
+++ WigglyCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WigglyCollectionView
+//
+//  Created by Grzegorz Tatarzyn on 23/02/2016.
+//  Copyright © 2016 Grzegorz Tatarzyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
